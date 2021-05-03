f40800f32e6c037153302353414e2af422dec3a5
Sergey7111968 <050Ser09@i.ua>
2021-05-03 12:16:51 +0300

    Lesson_3

---
+++ Lesson_3/main.py
@@ -0,0 +1,28 @@
+#print("I'm a string")
+#print('''I'm a "string"''')
+#print('I am a "string"')
+#print('I\'m a \'\'\'  "string"')
+
+# print(True)
+# print(False)
+
+# print(True>False)
+# print(True<False)
+
+# print(None)
+
+# a=10
+# b=20
+
+# print('a =',a, '| b =',b)
+
+var=1
+account_balance=1000.0
+client_name='Jone Doe'
+
+print(var,account_balance,client_name )
+
+account_balance=2000.0
+account_balance=account_balance + 700
+
+print(var,account_balance,client_name )
