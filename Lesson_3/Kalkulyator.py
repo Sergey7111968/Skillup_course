82375483728419f797047328ad31ba7c65dcdb91
Sergey7111968 <050Ser09@i.ua>
2021-05-04 08:28:59 +0300

    Lesson_3

---
+++ Lesson_3/Kalkulyator.py
@@ -0,0 +1,12 @@
+print('Enter the first number')
+a=int(input())
+print('Enter the second number')
+b=int(input())
+s=a+b
+r=a-b
+p=a*b
+d=a/b
+print('Sum =',s)
+print('Difference =',r)
+print('Multiply =',p)
+print('Divide',d)
