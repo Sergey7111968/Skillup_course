332a5d236af516d66d9068245b6b5e38b9a152cd
Sergey7111968 <050Ser09@i.ua>
2021-05-06 14:29:09 +0300

    DZ-2.2

---
+++ Lesson_2.2/DZ_2.2.py
@@ -0,0 +1,37 @@
+
+first_number=float(input('Enter first number :'))
+second_number=float(input('Enter second number :'))
+third_number=float(input('Enter third number :'))
+sign=input('Enter (+, -) :')
+
+if sign=='+':
+    suma=first_number+second_number+third_number
+    print(suma)
+elif sign=='-':
+    difference=first_number-second_number-third_number
+    print(difference)
+
+
+# first_number=float(input('Enter first number :'))
+# second_number=float(input('Enter second number :'))
+# third_number=float(input('Enter third number :'))
+# sign=input('Enter (<, ->) :')
+
+# if sign=='>':
+#     Max=max(first_number, second_number, third_number)
+#     print(Max)
+# elif sign=='<':
+#     Min=min(first_number, second_number, third_number)
+#     print(Min)
+
+
+##number_meters=float(input('Enter number meters :' ))
+##cm_mm=input('Enter (сm, mm) :')
+##
+##if cm_mm=='cm':
+##    number_cm=number_meters*100
+##    print(number_cm)
+##elif cm_mm=='mm':
+##    number_mm=number_meters*1000
+##    print(number_mm)
+
---
+++ Lesson_2.2/Lesson_2.2.py
@@ -0,0 +1,58 @@
+# print('Start typing below...')
+# user_input=input('Enter your text: ')
+# print("U've finished typing")
+# print('Text is:',user_input)
+
+# print('Start typing below...')
+
+# a=float(input('Enter a: '))
+# b=int(input('Enter b: '))
+
+# print("A + B =", a+b)
+
+# Collections
+# list_1=[1, 2, 3, 4, 5]
+# list_2=['Hello', 'World']
+# list_3=[1, 'strin', 12.4, True, False]
+
+# full_list=[list_1[0], list_2[1], list_3[2]]
+# print(full_list)
+
+# Dicts
+# footballers_names=['F1', 'F2', 'F3', 'F11']
+# footballers_ages=[25, 32, 22, 19]
+
+# print(footballers_names)
+# print(footballers_ages)
+
+# from pprint import pprint
+
+# dd={
+#     'F1': {
+#         'name': "F1_name",
+#         'age': 21,
+#         'weight': 65
+#     },
+#     'F2': {
+#         'name': "F2_name",
+#         'age': 32,
+#         'weight': 75
+#     },
+       
+# }
+# # print(dd)
+
+# print(dd['F1']['weight'])
+
+# if/else
+ 
+name=(input('Enter your name: '))
+age=int(input('Enter your age: '))
+valid_age=21
+
+if age>=valid_age:
+    print('You are welcome')
+else:
+    print('Sorry, you are too young')
+print('--------')
+
